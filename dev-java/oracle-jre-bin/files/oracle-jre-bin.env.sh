d74488a597ec8f3ec31b303d8917a82ef5109ab4
Daniel Robbins <drobbins@funtoo.org>
2017-12-29 02:58:56 +0000

    updates
    merged:
    
      flora: f14f075ad7e6949833cf78b342d6ec892aaaa616
      faustoo: 6f1879514e953595fbbda85b4a9feefdf976ce20
      fusion809: 1dd68f1d15d20bf4b64adb514cdab9d86d8063fa
      rh1: f8694e6c9c536da79b5be8dfe705d14723120df5
      gentoo-staging: 785f6452f43b33c47b99826bedf156b520f4e905
      kit-fixups: 42f72c2317153394363183249347155ac158e77f

--- dev-java/oracle-jre-bin/files/oracle-jre-bin.env.sh
+++ dev-java/oracle-jre-bin/files/oracle-jre-bin.env.sh
@@ -1,13 +1,12 @@
 # Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Id$
 
 VERSION="Oracle JRE ${PV}"
 JAVA_HOME="${EPREFIX}/opt/${P}"
 JDK_HOME="${EPREFIX}/opt/${P}"
 JAVAC="\${JAVA_HOME}/bin/javac"
-PATH="\${JAVA_HOME}/bin:\${JAVA_HOME}/bin"
-ROOTPATH="\${JAVA_HOME}/bin:\${JAVA_HOME}/bin"
+PATH="\${JAVA_HOME}/bin"
+ROOTPATH="\${JAVA_HOME}/bin"
 LDPATH="\${JAVA_HOME}/lib/$(get_system_arch)/:\${JAVA_HOME}/lib/$(get_system_arch)/server/"
 MANPATH="${EPREFIX}/opt/${P}/man"
 PROVIDES_TYPE="JRE"
--- virtual/jre/jre-1.8.0-r1.ebuild
+++ virtual/jre/jre-1.8.0-r1.ebuild
@@ -1,12 +1,13 @@
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI="5"
+EAPI="6"
 
 DESCRIPTION="Virtual for Java Runtime Environment (JRE)"
 SLOT="1.8"
-KEYWORDS="*"
+KEYWORDS="amd64 ~arm ~arm64 ppc64 x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc64-solaris ~x64-solaris"
 
 RDEPEND="|| (
-		=dev-java/oracle-jre-bin-1.8.0*
-		=virtual/jdk-1.8.0*
+		virtual/jdk:1.8
+		dev-java/oracle-jre-bin:1.8
 	)"
